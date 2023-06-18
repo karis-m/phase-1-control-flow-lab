a3c480f57bd16cb3c437f4796e097c5521bec21f
moses <moses.kamau@student.moringaschool.com>
2023-06-18 14:21:54 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+let someNumber = 199;
+let city;
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
-}
+  let result;
+  if(someNumber <= 400)
+  {result = 'This one is on me!';}
+  else if (someNumber > 2000)
+  {result = 'I will gladly take your thirty bucks.';}
+  else if(someNumber > 2500)
+  {result = 'No can do';}
+  return result;}
+  
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+let result;
+if(city = "NYC"){
+result = 'Ok, sounds good.';}
+else
+{result = 'No go.';}
+return result;
 }
-
-function switchOnCharmFromTip(){
+let tip;
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result;
+  if(tip = 'generous')
+  {result = 'Thank you so much.';}
+  else if(tip = 'not as generous')
+  {result = 'Thank you.';}
+  else
+  {result = 'Bye.';}
+  return result;
+
 }
